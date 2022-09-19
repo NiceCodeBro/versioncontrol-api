90102292d6d780fae08b85371a23439d19108826
Selim Dursun <m.seldur@gmail.com>
2022-09-19 23:50:26 +0200

    Make some validations in usecase

--- src/modules/version-control/use-cases/get-most-popular-repositories.ts
+++ src/modules/version-control/use-cases/get-most-popular-repositories.ts
@@ -1,4 +1,5 @@
 import { IGetAllPublicReposRequest, IGetMostPopularReposResponse } from ".";
+import { InputValidationError } from "../../../shared/error-types/input-validation-error";
 import { IUseCase } from "../../../shared/use-case-interface";
 import { IVersionControlRepository } from "../repositories";
 
@@ -7,14 +8,24 @@ export class GetMostPopularReposUseCase implements
   constructor(private _versionControlRepository: IVersionControlRepository) {}
   
   async execute(request: IGetAllPublicReposRequest): Promise<IGetMostPopularReposResponse> {     
-    // validate perPage type and either 10-30-50
-       
+    this.validateRequest(request);
+    
     const repos = await this._versionControlRepository.getMostPopularRepos(request);
 
-
-    if (typeof request.languageFilter === 'string' && request.languageFilter.length > 0) {
+    if (this.filterByLanguageActive(request.languageFilter)) {
       return { repositories: repos.repositories.filter(repo => repo.language === request.languageFilter) }
     }
     return repos;
+  }
+
+  validateRequest = (request:IGetAllPublicReposRequest) => {
+    // Per page can only be one of 10,50 or 100 if it is used
+    if (request.perPage !== undefined && ![10,50,100].find(n => n === request.perPage)){
+      throw new InputValidationError();
+    }
+  }
+
+  filterByLanguageActive = (languageFilter:string | undefined) => {
+    return typeof languageFilter === 'string' && languageFilter.length > 0;
   }
 }
